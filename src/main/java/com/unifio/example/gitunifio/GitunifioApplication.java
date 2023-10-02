cecb24522bbdfc86164b47ae2133dbd3b3da797e
thiago.paiva <thiago.paiva@totvs.com.br>
2023-10-01 23:58:20 -0300

    inicio

---
+++ src/main/java/com/unifio/example/gitunifio/GitunifioApplication.java
@@ -0,0 +1,13 @@
+package com.unifio.example.gitunifio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitunifioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitunifioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifio/example/gitunifio/GitunifioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifio.example.gitunifio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitunifioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
